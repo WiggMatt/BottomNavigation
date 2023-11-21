941a2ed0331656bd7ed2681e81fd99018df64ac8
Matthew <defender_0000@mail.ru>
2023-11-21 22:56:34 +0300

    Finish commit.

--- app/src/main/java/com/example/bottomnavigationdemo/LibraryFragment.java
+++ app/src/main/java/com/example/bottomnavigationdemo/LibraryFragment.java
@@ -7,14 +7,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class LibraryFragment extends Fragment {
 
+    private ListView listViewCart;
+    private ArrayAdapter<String> adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_library, container, false);
+
+        // Получаем ссылку на ListView
+        listViewCart = rootView.findViewById(R.id.listViewCart);
+
+        // Создаем адаптер и устанавливаем его для ListView
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, getDishesForCountry("china"));
+        listViewCart.setAdapter(adapter);
+
+        // Остальной код...
+
+        return rootView;
+    }
+
+    // Метод для получения массива блюд по стране
+    private String[] getDishesForCountry(String country) {
+        int arrayResourceId = getResources().getIdentifier("dishes_" + country, "array", requireContext().getPackageName());
+        return getResources().getStringArray(arrayResourceId);
+    }
+
+    public void onOrderButtonClick(View view) {
+        Toast.makeText(requireContext(), "Кнопка 'Заказать' недоступна в этой версии приложения", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/bottomnavigationdemo/SubscriptionFragment.java
+++ app/src/main/java/com/example/bottomnavigationdemo/SubscriptionFragment.java
@@ -7,13 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class SubscriptionFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subscription, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_subscription, container, false);
+
+        CalendarView calendarView = rootView.findViewById(R.id.calendarView);
+        Button bookButton = rootView.findViewById(R.id.bookButton);
+
+        // Обработка события нажатия на кнопку
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Здесь добавьте код для обработки нажатия на кнопку "Забронировать"
+                // Например, показать диалоговое окно с подтверждением бронирования
+                Toast.makeText(getContext(), "Бронирование не доступно в данной версии приложения", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
     }
+
 }
